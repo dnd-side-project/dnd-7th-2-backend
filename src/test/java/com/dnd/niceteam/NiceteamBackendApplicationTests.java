d65630ec8d327a4e08b6c3207e58082aa7fbda74
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-12 02:39:10 +0900

    프로젝트 세팅

---
+++ src/test/java/com/dnd/niceteam/NiceteamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.niceteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceteamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
