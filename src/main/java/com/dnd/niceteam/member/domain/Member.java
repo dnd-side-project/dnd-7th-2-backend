cdb30f885ee7d796698049708da5fb6276884c95
banan99 <banan99@naver.com>
2022-07-15 12:02:41 +0900

    [FEAT] member domain & role

--- src/main/java/com/dnd/niceteam/member/domain/Member.java
+++ src/main/java/com/dnd/niceteam/member/domain/Member.java
@@ -1,2 +1,49 @@
-package com.dnd.niceteam.member.domain;public class Member {
+package com.dnd.niceteam.member.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="MEMBER")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD", length = 60)
+    private String password;
+
+    @Column(name="ROLE", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ACCESS_TOKEN")
+    private String accessToken;
+
+    @Column(name = "REFRESH_TOKEN")
+    private String refreshToken;
+
+    public void updateAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setTokenInfo(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
--- src/main/java/com/dnd/niceteam/member/domain/Role.java
+++ src/main/java/com/dnd/niceteam/member/domain/Role.java
@@ -1,4 +1,16 @@
 package com.dnd.niceteam.member.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
+    USER("USER","ROLE_USER"),ADMIN("ADMIN","ROLE_ADMIN");
+
+    private final String name;
+    private final String fullName;
+
+    Role ( String name, String fullName) {
+        this.name = name;
+        this.fullName = fullName;
+    }
 }
