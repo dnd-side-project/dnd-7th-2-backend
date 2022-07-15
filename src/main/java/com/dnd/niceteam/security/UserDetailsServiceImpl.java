c784e8e490c0846c9612c9a7b6d06d0f415cbb56
banan99 <banan99@naver.com>
2022-07-15 12:56:02 +0900

    [FEAT] UserDetails* 구현체 생성

--- src/main/java/com/dnd/niceteam/security/UserDetailsServiceImpl.java
+++ src/main/java/com/dnd/niceteam/security/UserDetailsServiceImpl.java
@@ -1,24 +1,53 @@
 package com.dnd.niceteam.security;
 
+import com.dnd.niceteam.member.domain.Member;
 import com.dnd.niceteam.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * DB에서 요청 유저의 인증 정보를 가져온다.
+ * Class       : UserDetailsImpl
+ * Author      : 문 윤지
+ * Description : UserDetailsService 구현체
+ * History     : [2022-07-15] 문윤지 - Class Create
  */
+
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+        UserDetailsImpl user = findSecurityUserByUsername(username);
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getAuthorities());
+    }
 
+    // security user 조회
+    public UserDetailsImpl findSecurityUserByUsername(String username) {
+        // TODO: 2022-07-15 error throw
+        Member member = memberRepository.findOneByUsername(username)
+                .orElseThrow();
+        return convertToUserDetails(member);
+    }
+
+    private UserDetailsImpl convertToUserDetails(Member member) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(member.getRole().getFullName()));
+
+        return new UserDetailsImpl(
+                member.getUsername(),
+                member.getPassword(),
+                grantedAuthorities
+        );
     }
 }
