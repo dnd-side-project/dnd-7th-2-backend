a57740c7ee049e21536d4b69c5d75b769b97315c
banan99 <banan99@naver.com>
2022-07-18 01:04:50 +0900

    [FIXT] auth 패키지 이동 및 에러&반환클래스 적용

--- src/main/java/com/dnd/niceteam/security/auth/controller/AuthController.java
+++ src/main/java/com/dnd/niceteam/security/auth/controller/AuthController.java
@@ -1,2 +1,48 @@
-package com.dnd.niceteam.security.auth.controller;public class AuthController {
+package com.dnd.niceteam.security.auth.controller;
+
+import com.dnd.niceteam.common.dto.ApiResult;
+import com.dnd.niceteam.member.dto.MemberResponseDto;
+import com.dnd.niceteam.member.service.MemberService;
+import com.dnd.niceteam.security.auth.dto.AuthRequestDto;
+import com.dnd.niceteam.security.auth.dto.AuthResponseDto;
+import com.dnd.niceteam.security.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody AuthRequestDto.Login loginRequestDto) {
+        AuthResponseDto.TokenInfo login = authService.login(loginRequestDto);
+        ApiResult<AuthResponseDto.TokenInfo> apiResult = ApiResult.success(login);
+
+        return new ResponseEntity<>(apiResult, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(@Valid @RequestBody AuthRequestDto.Reissue reissueRequestDto) {
+        AuthResponseDto.Reissue reissue = authService.reissueAccessToken(reissueRequestDto);
+        ApiResult<AuthResponseDto.Reissue> apiResult = ApiResult.success(reissue);
+
+        return new ResponseEntity<>(apiResult, HttpStatus.CREATED);
+    }
+
+    // NOTICE. header에 Access Token
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(String username) {
+        authService.logout(username);
+
+        return new ResponseEntity<>(ApiResult.success(), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/dnd/niceteam/security/auth/dto/AuthResponseDto.java
+++ src/main/java/com/dnd/niceteam/security/auth/dto/AuthResponseDto.java
@@ -1,2 +1,20 @@
-package com.dnd.niceteam.security.auth.dto;public class AuthResponseDto {
+package com.dnd.niceteam.security.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+public class AuthResponseDto {
+    @Data
+    @AllArgsConstructor
+    public static class TokenInfo {
+        private String accessToken;
+        private String refreshToken;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Reissue {
+        private String accessToken;
+    }
 }
