6806b065e1f65dece0dde64bfdba32844cc24a28
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-17 13:10:56 +0900

    docs: rest docs 틀 잡기

---
+++ src/test/java/com/dnd/niceteam/common/CommonDocsController.java
@@ -0,0 +1,27 @@
+package com.dnd.niceteam.common;
+
+import com.dnd.niceteam.common.dto.ApiResult;
+import com.dnd.niceteam.error.dto.ErrorResponseDto;
+import com.dnd.niceteam.error.exception.ErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docs/common")
+public class CommonDocsController {
+
+    @GetMapping("/api-result")
+    public ResponseEntity<ApiResult<String>> apiResult() {
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.of(
+                ErrorCode.INVALID_INPUT_VALUE,
+                ErrorResponseDto.FieldError.of("input-field", "input-value", "error-reason"));
+        ApiResult<String> result = ApiResult.<String>builder()
+                .data("response-data")
+                .status(ApiResult.Status.SUCCESS)
+                .error(errorResponseDto)
+                .build();
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ src/test/java/com/dnd/niceteam/common/CommonDocsControllerTest.java
@@ -0,0 +1,70 @@
+package com.dnd.niceteam.common;
+
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import({RestDocsConfig.class, CommonDocsControllerTest.TestSecurityConfig.class})
+@WebMvcTest(CommonDocsController.class)
+@AutoConfigureRestDocs
+class CommonDocsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void apiResult() throws Exception {
+        mockMvc.perform(get("/docs/common/api-result")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer authentication-token"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("common-ApiResult",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("인증이 필요한 요청의 경우 해당 헤더에 인증 토큰을 포함")
+                        ),
+                        responseFields(
+                            fieldWithPath("status").description("API 응답 상태"),
+                                fieldWithPath("data").type(Object.class).optional()
+                                        .description("응답 결과 데이터\n- 응답이 실패한 경우 포함되지 않음"),
+                                fieldWithPath("error").optional()
+                                        .description("에러 결과\n- 에러가 없는 경우 포함되지 않음"),
+                                fieldWithPath("error.code").description("에러코드 - 규칙: Domain-Num"),
+                                fieldWithPath("error.message").description("에러 메세지"),
+                                fieldWithPath("error.errors").optional()
+                                        .description("요청 필드에 대한 예외 상황들\n- 필드 예외가 없는 경우 빈 배열 []"),
+                                fieldWithPath("error.errors[].field").description("예외가 발생한 요청 필드"),
+                                fieldWithPath("error.errors[].value").description("예외가 발생한 요청 값"),
+                                fieldWithPath("error.errors[].reason").description("예외가 발생한 원인")
+                        )
+                ))
+        ;
+    }
+
+    @TestConfiguration
+    static class TestSecurityConfig {
+
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return web -> web.ignoring().antMatchers("/docs/**");
+        }
+    }
+}
