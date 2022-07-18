e880975002c89a819f2ead2c2890b13dd94465db
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-18 17:02:16 +0900

    fix: Member 칼럼 세부사항 명시 및 password 길이 수정
    spring security 의 BCryptPasswordEncoder 사용시 앞에 {bcrypt} 가 붙어서 길이가 68이 됨.
    여유 공간을 고려하여 password 길이 수정

--- src/main/java/com/dnd/niceteam/member/domain/Member.java
+++ src/main/java/com/dnd/niceteam/member/domain/Member.java
@@ -16,26 +16,24 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "username", length = 45, unique = true, nullable = false)
     private String username;
 
-    @Column(name = "password", length = 60)
+    @Column(name = "password", length = 105, nullable = false)
     private String password;
 
-    @Column(name="role", nullable = false)
+    @Column(name="role", length = 25, nullable = false)
     @Enumerated(EnumType.STRING)
-    private Role role;
+    @Builder.Default
+    private Role role = Role.USER;
 
-    @Column(name = "email")
+    @Column(name = "email", length = 65, unique = true, nullable = false)
     private String email;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 25, nullable = false)
     private String name;
 
     @Column(name = "refresh_token")
+    @Setter
     private String refreshToken;
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
 }
