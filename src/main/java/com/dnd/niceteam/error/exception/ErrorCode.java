546cdbe44f39a242c5984f6d1174a43a8581b90e
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-28 01:12:44 +0900

    refactor: ErrorCode.HANDLE_UNAUTHORIZED codeNum 수정

--- src/main/java/com/dnd/niceteam/error/exception/ErrorCode.java
+++ src/main/java/com/dnd/niceteam/error/exception/ErrorCode.java
@@ -16,10 +16,10 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(COMMON, 3, SC_BAD_REQUEST, "유효하지 않은 입력 값입니다."),
     INVALID_TYPE_VALUE(COMMON, 4, SC_BAD_REQUEST,  "유효하지 않은 타입입니다."),
 
-    REFRESH_TOKEN_IS_NULL_ERROR(AUTH, 1, SC_UNAUTHORIZED, "로그아웃 상태입니다."),
+    HANDLE_UNAUTHORIZED(AUTH, 1, SC_UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     INVALID_TOKEN(AUTH, 2, SC_BAD_REQUEST, "유효하지 않은 토큰입니다."),
     USERNAME_NOT_FOUND(AUTH, 3, SC_NOT_FOUND, "존재하지 않는 아이디 입니다."),
-    HANDLE_UNAUTHORIZED(AUTH, 4, SC_UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    REFRESH_TOKEN_IS_NULL_ERROR(AUTH, 4, SC_UNAUTHORIZED, "로그아웃 상태입니다."),
 
     MEMBER_NOT_FOUND(MEMBER, 1, SC_NOT_FOUND, "존재하지 않는 회원입니다."),
     ;
