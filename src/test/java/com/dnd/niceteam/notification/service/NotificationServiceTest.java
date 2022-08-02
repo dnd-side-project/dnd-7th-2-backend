d81ae882d1f0179f8a87bc4000eb3ac6bfcdf48a
m-dzn <88yangkh@gmail.com>
2022-08-03 08:27:01 +0900

    test(Notification): Service 슬라이스 테스트 작성 : 푸쉬 알람 전송 테스트

---
+++ src/test/java/com/dnd/niceteam/notification/service/NotificationServiceTest.java
@@ -0,0 +1,41 @@
+package com.dnd.niceteam.notification.service;
+
+import com.dnd.niceteam.notification.dto.NotificationRequestDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+    @InjectMocks
+    NotificationService notificationService;
+
+    @Mock
+    FirebaseCloudMessagingService fcmService;
+
+    @Test
+    void 푸쉬_알람_전송() throws ExecutionException, InterruptedException {
+        // given
+        NotificationRequestDto request = new NotificationRequestDto();
+        request.setTitle("팀원 수락");
+        request.setMessage("DND 7기 2조에 합류하신걸 축하합니다!");
+
+        when(fcmService.sendMessageToToken(request)).thenReturn("푸쉬 알람 전송 성공");
+
+        // when
+        notificationService.send(request);
+
+        // then
+        verify(fcmService).sendMessageToToken(request);
+    }
+
+
+}
