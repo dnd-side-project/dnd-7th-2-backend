99ec63f6b992d4df536f97ec749614acba23b560
Kyeongho Yang <mdzn.dev@gmail.com>
2022-08-04 07:22:28 +0900

    feat(Project): 프로젝트 Entity 추가

---
+++ src/main/java/com/dnd/niceteam/domain/project/Project.java
@@ -0,0 +1,44 @@
+package com.dnd.niceteam.domain.project;
+
+import com.dnd.niceteam.domain.common.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "project")
+@SQLDelete(sql = "UPDATE project SET use_yn = false WHERE id = ?")
+@Where(clause = "use_yn = true")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProjectType type;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private ProjectStatus status = ProjectStatus.NOT_STARTED;
+
+}
