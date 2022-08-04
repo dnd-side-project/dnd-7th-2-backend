8441cb07c0e16db267b294f05185ebd3b0ad2d5b
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-04 18:25:37 +0900

    feat: MemberRepository findByEmail

--- src/main/java/com/dnd/niceteam/domain/member/MemberRepository.java
+++ src/main/java/com/dnd/niceteam/domain/member/MemberRepository.java
@@ -2,7 +2,7 @@ package com.dnd.niceteam.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     boolean existsByNickname(String nickname);
 }
