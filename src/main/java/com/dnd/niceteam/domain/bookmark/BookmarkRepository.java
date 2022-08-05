27f984557bfd7ddcd8e7e9093279b444d184c558
banan99 <banan99@naver.com>
2022-08-05 17:45:08 +0900

    feat(Recruiting): Repository 추가

---
+++ src/main/java/com/dnd/niceteam/domain/bookmark/BookmarkRepository.java
@@ -0,0 +1,6 @@
+package com.dnd.niceteam.domain.bookmark;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
