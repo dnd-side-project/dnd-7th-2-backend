f866a69607117ffe4d673e68a8f2d015d85f780a
banan99 <banan99@naver.com>
2022-08-05 15:50:48 +0900

    feat(ActivityDayTime): 활동일자 Entity 추가

---
+++ src/main/java/com/dnd/niceteam/domain/recruiting/ActivityDayTime.java
@@ -0,0 +1,41 @@
+package com.dnd.niceteam.domain.recruiting;
+
+import com.dnd.niceteam.domain.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE activity_day_time SET use_yn = false WHERE id = ?")
+@Where(clause = "use_yn = true")
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "activity_day_time")
+public class ActivityDayTime extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "activity_day_time_id")
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "recruiting_id", nullable = false)
+    private Recruiting recruiting;
+
+    //'월'~'금'
+    @Column(nullable = false)
+    private char day;
+
+    @Column(name = "start_time", nullable = false)
+    private Time startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private Time endTime;
+
+}
+
