3280c2819e18d316fd48ce4df3d25837a155fc0c
Kyeongho Yang <mdzn.dev@gmail.com>
2022-08-07 22:06:11 +0900

    feat(Vote): 투표 그룹 상속 Entity 추가

---
+++ src/main/java/com/dnd/niceteam/domain/vote/VoteGroupToCompleteProject.java
@@ -0,0 +1,26 @@
+package com.dnd.niceteam.domain.vote;
+
+import com.dnd.niceteam.domain.project.Project;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("COMPLETE_PROJECT")
+@OnDelete(action = OnDeleteAction.CASCADE)
+public class VoteGroupToCompleteProject extends VoteGroup {
+
+    @Builder
+    public VoteGroupToCompleteProject(Project project) {
+        super(project);
+    }
+
+}
