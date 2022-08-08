a67521765a8c588432667de676420a45cb024e61
banan99 <banan99@naver.com>
2022-08-09 00:46:42 +0900

    feat(ErrorCode): 프로젝트, 모집글 에러코드 추가

--- src/main/java/com/dnd/niceteam/error/exception/ErrorCode.java
+++ src/main/java/com/dnd/niceteam/error/exception/ErrorCode.java
@@ -34,9 +34,12 @@ public enum ErrorCode {
     DEPARTMENT_NOT_FOUND(DEPARTMENT, SC_NOT_FOUND, "존재하지 않는 학과입니다."),
 
     // Project
-    INVALID_PROJECT_SCHEDULE(PROJECT, SC_BAD_REQUEST, "프로젝트 기간이 유효하지 않습니다.")
-    ;
+    INVALID_PROJECT_SCHEDULE(PROJECT, SC_BAD_REQUEST, "프로젝트 기간이 유효하지 않습니다."),
+    PROJECT_NOT_FOUND(PROJECT, SC_NOT_FOUND, "존재하지 않는 프로젝트입니다."),
 
+    // Recruiting
+    INVALID_RECRUITING_TYPE(RECRUITING, SC_BAD_REQUEST, "모집글 타입이 유효하지 않습니다."),
+    ;
     private final Domain domain;
 
     private final int status;
