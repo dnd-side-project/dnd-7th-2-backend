9ce670df716d9d29b6f6cbf701c8ebfa0f8ea4a4
Kyeongho Yang <mdzn.dev@gmail.com>
2022-08-10 23:07:26 +0900

    feat: 페이지네이션 공통 DTO 추가

---
+++ src/main/java/com/dnd/niceteam/common/dto/Pagination.java
@@ -0,0 +1,46 @@
+package com.dnd.niceteam.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@ToString
+public class Pagination<T> {
+
+    private final int page;
+
+    private final int perSize;
+
+    private final int totalCount;
+
+    private final int totalPages;
+
+    private final boolean prev;
+
+    private final boolean next;
+
+    private final List<T> contents;
+
+    @Builder
+    private Pagination(int page, int perSize, int totalCount, List<T> contents) {
+        this.page = page;
+        this.perSize = perSize;
+        this.totalCount = totalCount;
+
+        this.contents = Objects.requireNonNullElseGet(contents, ArrayList::new);
+
+        // 연산된 값
+        int pageWithFullContent = totalCount / perSize;
+        int pageWithLackContent = totalCount % perSize == 0 ? 1 : 0;
+        this.totalPages = pageWithFullContent + pageWithLackContent;
+
+        this.prev = page > 1;
+        this.next = page < totalPages;
+    }
+
+}
