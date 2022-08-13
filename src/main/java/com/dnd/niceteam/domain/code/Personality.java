c75bb82f152c7fbfde160073837637676fc9b7a6
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-13 19:25:15 +0900

    feat: Personality 성향 코드 EnumMapper 적용

--- src/main/java/com/dnd/niceteam/domain/code/Personality.java
+++ src/main/java/com/dnd/niceteam/domain/code/Personality.java
@@ -1,9 +1,7 @@
 package com.dnd.niceteam.domain.code;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.dnd.niceteam.domain.common.EnumMapperType;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -24,27 +22,55 @@ public class Personality {
     @Column(name = "personality_noun", length = 15, nullable = false)
     private Noun noun;
 
-    public enum Adjective {
-        LOGICAL,        // 논리적인
-        PRECISE,        // 꼼꼼한
-        PLANNED,        // 계획적인
-        QUICK_WORKING,  // 일처리가 빠른
-        CHEERFUL,       // 쾌활한
-        CREATIVE,       // 창의적인
-        SINCERE,        // 성실한
-        GOAL_ORIENTED,  // 목표지향적인
-        PERSISTENT,     // 끈기있는
+    @RequiredArgsConstructor
+    public enum Adjective implements EnumMapperType {
+        LOGICAL("논리적인"),
+        PRECISE("꼼꼼한"),
+        PLANNED("계획적인"),
+        QUICK_WORKING("일처리가 빠른"),
+        CHEERFUL("쾌활한"),
+        CREATIVE("창의적인"),
+        SINCERE("성실한"),
+        GOAL_ORIENTED("목표지향적인"),
+        PERSISTENT("끈기있는"),
+        ;
+
+        private final String title;
+
+        @Override
+        public String getCode() {
+            return name();
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
     }
 
-    public enum Noun {
-        INVENTOR,               // 발명가
-        LEADER,                 // 리더
-        FOLLOWER,               // 팔로워
-        PERFECTIONIST,          // 완벽주의자
-        COMMUNICATOR,           // 커뮤니케이터
-        ADVENTURER,             // 모험가
-        ANALYST,                // 분석가
-        MEDIATOR,               // 중재자
-        JACK_OF_ALL_TRADES,   // 만능재주꾼
+    @RequiredArgsConstructor
+    public enum Noun implements EnumMapperType {
+        INVENTOR("발명가"),
+        LEADER("리더"),
+        FOLLOWER("팔로워"),
+        PERFECTIONIST("완벽주의자"),
+        COMMUNICATOR("커뮤니케이터"),
+        ADVENTURER("모험가"),
+        ANALYST("분석가"),
+        MEDIATOR("중재자"),
+        JACK_OF_ALL_TRADES("만능재주꾼"),
+        ;
+
+        private final String title;
+
+        @Override
+        public String getCode() {
+            return name();
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
     }
 }
