9078734034ecb8b729d1556ba95abfc525228cd8
Kyeongho Yang <mdzn.dev@gmail.com>
2022-08-15 13:42:39 +0900

    feat(Project): 프로젝트 수정 Service 메서드 추가

---
+++ src/main/java/com/dnd/niceteam/domain/project/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.dnd.niceteam.domain.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
