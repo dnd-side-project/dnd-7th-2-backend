d5fbbc7a4173e585cc7cfc31e9d4d69dde39126b
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-17 04:25:02 +0900

    test: 북마크 제거 API

--- src/test/java/com/dnd/niceteam/bookmark/controller/BookmarkControllerTest.java
+++ src/test/java/com/dnd/niceteam/bookmark/controller/BookmarkControllerTest.java
@@ -1,8 +1,10 @@
 package com.dnd.niceteam.bookmark.controller;
 
 import com.dnd.niceteam.bookmark.dto.BookmarkCreation;
+import com.dnd.niceteam.bookmark.dto.BookmarkDeletion;
 import com.dnd.niceteam.bookmark.service.BookmarkService;
 import com.dnd.niceteam.common.RestDocsConfig;
+import com.dnd.niceteam.error.exception.ErrorCode;
 import com.dnd.niceteam.security.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,10 +19,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -68,5 +70,55 @@ class BookmarkControllerTest {
                                 fieldWithPath("id").description("북마크 ID")
                         )
                 ));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("북마크 삭제 API")
+    void bookmarkDelete() throws Exception {
+        // given
+        long givenBookmarkId = 1L;
+        BookmarkDeletion.ResponseDto responseDto = new BookmarkDeletion.ResponseDto();
+        responseDto.setId(givenBookmarkId);
+        given(mockBookmarkService.isBookmarkOwnedByMember(eq(givenBookmarkId), anyString())).willReturn(true);
+        given(mockBookmarkService.deleteBookmark(givenBookmarkId)).willReturn(responseDto);
+
+        // expected
+        mockMvc.perform(delete("/bookmarks/{bookmarkId}", givenBookmarkId)
+                        .with(csrf())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.id").isNumber())
+                .andDo(document("bookmark-delete",
+                        pathParameters(
+                                parameterWithName("bookmarkId").description("북마크 ID")
+                        ),
+                        responseFields(
+                                beneathPath("data").withSubsectionId("data"),
+                                fieldWithPath("id").description("북마크 ID")
+                        )
+                ));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("북마크 삭제 API - 소유하지 않은 북마크")
+    void bookmarkDelete_BookmarkNotOwned() throws Exception {
+        // given
+        given(mockBookmarkService.isBookmarkOwnedByMember(anyLong(), anyString())).willReturn(false);
+
+        // expected
+        mockMvc.perform(delete("/bookmarks/{bookmarkId}", 1L)
+                        .with(csrf())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value(ErrorCode.BOOKMARK_NOT_OWNED.name()))
+                .andExpect(jsonPath("$.error.message").value(ErrorCode.BOOKMARK_NOT_OWNED.getMessage()))
+                .andExpect(jsonPath("$.error.errors").isEmpty())
+        ;
     }
 }
