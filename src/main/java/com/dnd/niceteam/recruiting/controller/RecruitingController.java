00f87437c62a5960dc14f10d8f84a90849e98af1
banan99 <banan99@naver.com>
2022-08-17 01:34:13 +0900

    feat(RecruitingController) : 모집글 등록 API 추가

--- src/main/java/com/dnd/niceteam/recruiting/controller/RecruitingController.java
+++ src/main/java/com/dnd/niceteam/recruiting/controller/RecruitingController.java
@@ -1,2 +1,27 @@
-package com.dnd.niceteam.recruiting.controller;public class RecruitingController {
+package com.dnd.niceteam.recruiting.controller;
+
+import com.dnd.niceteam.common.dto.ApiResult;
+import com.dnd.niceteam.recruiting.dto.RecruitingCreation;
+import com.dnd.niceteam.recruiting.service.RecruitingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/recruiting")
+@RestController
+public class RecruitingController {
+    private final RecruitingService recruitingService;
+
+    @PostMapping
+    public ResponseEntity<ApiResult<RecruitingCreation.ResponseDto>> recruitingAdd (@RequestBody RecruitingCreation.RequestDto recruitingReqDto) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        RecruitingCreation.ResponseDto responseDto = recruitingService.addProjectAndRecruiting(username, recruitingReqDto);
+
+        ApiResult<RecruitingCreation.ResponseDto> apiResult = ApiResult.success(responseDto);
+        return new ResponseEntity<>(apiResult, HttpStatus.CREATED);
+    }
+
 }
