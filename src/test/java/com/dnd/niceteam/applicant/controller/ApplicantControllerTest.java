376d2b0360ea1174bce4477844336beae2187c2c
banan99 <banan99@naver.com>
2022-08-18 23:46:53 +0900

    test(Applicant): Applicant 지원 취소 테스트 코드 추가

--- src/test/java/com/dnd/niceteam/applicant/controller/ApplicantControllerTest.java
+++ src/test/java/com/dnd/niceteam/applicant/controller/ApplicantControllerTest.java
@@ -1,6 +1,7 @@
 package com.dnd.niceteam.applicant.controller;
 
 import com.dnd.niceteam.applicant.dto.ApplicantCreation;
+import com.dnd.niceteam.applicant.service.ApplicantService;
 import com.dnd.niceteam.common.RestDocsConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -8,16 +9,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,36 +36,56 @@ class ApplicantControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @MockBean
+    private ApplicantService applicantService;
 
-    private static final Long MEMBER_ID = 1L;
     private static final Long RECRUITING_ID = 1L;
 
     @Test
     @WithMockUser
-    @DisplayName("모집글의 팀원 지원 API")
+    @DisplayName("모집 지원 등록 API")
     public void applicantCreate() throws Exception {
         // given
-        ApplicantCreation.RequestDto requestDto = new ApplicantCreation.RequestDto();
-        requestDto.setMemberId(MEMBER_ID);
+        ApplicantCreation.ResponseDto responseDto = new ApplicantCreation.ResponseDto();
+        responseDto.setId(1L);
+        given(applicantService.addApplicant(anyLong(), anyString())).willReturn(responseDto);
 
         // when
         ResultActions result = mockMvc.perform(
                 post("/recruiting/{recruitingId}/applicant", RECRUITING_ID)
                         .with(csrf())
                         .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDto))
         );
-
         // then
         result.andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.success").value(true))
                 .andDo(
                         document("applicant-create",
-                                requestFields(
-                                        fieldWithPath("memberId").description("회원 식별자 ID")
+                                responseFields(
+                                        beneathPath("data").withSubsectionId("data"),
+                                        fieldWithPath("id").description("지원자 식별자(ID)")
                                 )
+                        )
+                );
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("모집 지원 취소 API")
+    public void applicantDelete() throws Exception {
+        // when
+        ResultActions result = mockMvc.perform(
+                delete("/recruiting/{recruitingId}/applicant", RECRUITING_ID)
+                        .with(csrf())
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        // then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andDo(
+                        document("applicant-delete"
                         )
                 );
     }
