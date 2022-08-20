216e8514238147d71be3169b149e43daed6a7108
banan99 <banan99@naver.com>
2022-08-20 22:58:03 +0900

    refactor(RecruitingRepository): 조회 쿼리 메서드 수정

--- src/main/java/com/dnd/niceteam/domain/recruiting/RecruitingRepositoryCustomImpl.java
+++ src/main/java/com/dnd/niceteam/domain/recruiting/RecruitingRepositoryCustomImpl.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Set;
 
-import static com.dnd.niceteam.domain.member.QMember.member;
 import static com.dnd.niceteam.domain.project.QProject.project;
 import static com.dnd.niceteam.domain.project.QSideProject.sideProject;
 import static com.dnd.niceteam.domain.recruiting.QRecruiting.recruiting;
@@ -18,43 +17,6 @@ import static com.dnd.niceteam.domain.recruiting.QRecruiting.recruiting;
 @RequiredArgsConstructor
 public class RecruitingRepositoryCustomImpl implements RecruitingRepositoryCustom {
     private final JPAQueryFactory query;
-
-    @Override
-    public PageImpl<Recruiting> findAllByMemberId(Long memberId, Pageable pageable) {
-        List<Recruiting> recruitings = query
-                .selectFrom(recruiting)
-                .join(recruiting.member, member)
-                .where(member.id.eq(memberId))
-                .orderBy(recruiting.createdDate.desc())
-                .offset(pageable.getOffset()).limit(pageable.getPageSize())
-                .fetch();
-        Long totalCount = query.select(recruiting.count())
-                .from(recruiting)
-                .join(recruiting.member, member)
-                .where(member.id.eq(memberId))
-                .fetchOne();
-
-        return new PageImpl<>(recruitings, pageable, totalCount);
-    }
-
-    public PageImpl<Recruiting> findAllByMemberIdAndStatus(Long memberId, ProgressStatus progressStatus, Pageable pageable) {
-        List<Recruiting> recruitings = query
-                .selectFrom(recruiting)
-                .join(recruiting.member, member)
-                .where(member.id.eq(memberId)
-                        .and(recruiting.status.eq(progressStatus)))
-                .orderBy(recruiting.createdDate.desc())
-                .offset(pageable.getOffset()).limit(pageable.getPageSize())
-                .fetch();
-        Long totalCount = query.select(recruiting.count())
-                .from(recruiting)
-                .join(recruiting.member, member)
-                .where(member.id.eq(memberId)
-                        .and(recruiting.status.eq(progressStatus)))
-                .fetchOne();
-
-        return new PageImpl<>(recruitings, pageable, totalCount);
-    }
 
     @Override
     public PageImpl<Recruiting> findAllByInterestingFieldsOrderByWriterLevel(Set<Field> interestingFields, Pageable pageable) {
