23ffe81762543bb000bd4f3cf533429d02bc3826
Kyeongho Yang <mdzn.dev@gmail.com>
2022-08-22 01:34:26 +0900

    feat(Project): 멤버별 리뷰 여부 추가를 위한 DTO 수정

--- src/main/java/com/dnd/niceteam/project/dto/ProjectMemberResponse.java
+++ src/main/java/com/dnd/niceteam/project/dto/ProjectMemberResponse.java
@@ -2,6 +2,7 @@ package com.dnd.niceteam.project.dto;
 
 import com.dnd.niceteam.domain.member.Member;
 import com.dnd.niceteam.domain.project.ProjectMember;
+import com.dnd.niceteam.domain.review.MemberReview;
 import lombok.Data;
 
 public interface ProjectMemberResponse {
@@ -19,17 +20,28 @@ public interface ProjectMemberResponse {
 
         private boolean expelled;
 
-        public static Summary from(ProjectMember projectMember) {
+        private boolean reviewed = false;
+
+        public static Summary from (ProjectMember projectMember) {
+            return from(projectMember, null);
+        }
+
+        public static Summary from(ProjectMember projectMember, MemberReview memberReview) {
             Member member = projectMember.getMember();
             PersonalityResponse personalityResponse = PersonalityResponse.from(member.getPersonality());
 
             Summary dto = new Summary();
 
-            dto.setMemberId(projectMember.getId());
+            dto.setMemberId(projectMember.getMember().getId());
             dto.setNickname(member.getNickname());
             dto.setAdmissionYear(member.getAdmissionYear());
             dto.setPersonality(personalityResponse);
             dto.setExpelled(projectMember.getExpelled());
+
+            if (memberReview != null) {
+                boolean isMemberReviewed = projectMember.equals(memberReview.getReviewee());
+                dto.setReviewed(isMemberReviewed);
+            }
 
             return dto;
         }
--- src/main/java/com/dnd/niceteam/project/dto/ProjectResponse.java
+++ src/main/java/com/dnd/niceteam/project/dto/ProjectResponse.java
@@ -12,99 +12,10 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public interface ProjectResponse {
-
-    @Data
-    class Detail {
-
-        private Long id;
-
-        // Project 상태
-        private String name;
-        private Type type;
-
-        private LocalDate startDate;
-        private LocalDate endDate;
-
-        private ProjectStatus status;
-        private Integer memberCount;
-        private List<ProjectMemberResponse.Summary> memberList;
-
-        // Lecture Project 상태
-        private String professor;
-        private DepartmentResponse department;
-        private List<LectureTimeResponse> lectureTimes;
-
-        // Side Project
-        private Field field;
-        private FieldCategory fieldCategory;
-
-        // Auditing 정보
-        private LocalDateTime createdDate;
-        private LocalDateTime lastModifiedDate;
-        private String createdBy;
-        private String lastModifiedBy;
-
-        public static Detail from(Project project) {
-            return project.getType() == Type.LECTURE ?
-                    from((LectureProject) project) : from((SideProject) project);
-        }
-
-        public static Detail from(LectureProject lecture) {
-            Detail dto = new Detail();
-
-            List<LectureTimeResponse> lectureTimes = lecture.getLectureTimes().stream()
-                    .map(LectureTimeResponse::from).collect(Collectors.toList());
-
-            setProjectCommonDetails(lecture, dto);
-
-            // Lecture Project 상태
-            dto.setProfessor(lecture.getProfessor());
-            dto.setDepartment(DepartmentResponse.from(lecture.getDepartment()));
-            dto.setLectureTimes(lectureTimes);
-
-            return dto;
-        }
-
-        public static Detail from(SideProject side) {
-            Detail dto = new Detail();
-
-            setProjectCommonDetails(side, dto);
-
-            // Side Project 상태
-            dto.setField(side.getField());
-            dto.setFieldCategory(side.getFieldCategory());
-
-            return dto;
-        }
-
-        private static void setProjectCommonDetails(Project project, Detail dto) {
-            List<ProjectMemberResponse.Summary> memberList = project.getProjectMembers().stream()
-                    .map(ProjectMemberResponse.Summary::from).collect(Collectors.toList());
-
-            dto.setId(project.getId());
-
-            // Project 상태
-            dto.setName(project.getName());
-            dto.setType(project.getType());
-            dto.setStatus(project.getStatus());
-
-            dto.setStartDate(project.getStartDate());
-            dto.setEndDate(project.getEndDate());
-
-            dto.setMemberCount(project.getMemberCount());
-            dto.setMemberList(memberList);
-
-            // Auditing 정보
-            dto.setCreatedDate(project.getCreatedDate());
-            dto.setLastModifiedDate(project.getLastModifiedDate());
-            dto.setCreatedBy(project.getCreatedBy());
-            dto.setLastModifiedBy(project.getLastModifiedBy());
-        }
-
-    }
 
     @Data
     class ListItem {
@@ -147,11 +58,8 @@ public interface ProjectResponse {
             // 계산된 값
             dto.setMemberCount(project.getMemberCount());
 
-            if (project.getType() == Type.LECTURE) {
-                setLectureDetails((LectureProject) project, dto);
-            } else {
-                setSideDetails((SideProject) project, dto);
-            }
+            if (project instanceof LectureProject)          setLectureDetails((LectureProject) project, dto);
+            else                                            setSideDetails((SideProject) project, dto);
 
             return dto;
         }
@@ -170,6 +78,101 @@ public interface ProjectResponse {
             // Side Project 상태
             dto.setField(side.getField());
             dto.setFieldCategory(side.getFieldCategory());
+        }
+
+    }
+
+    @Data
+    class Detail {
+
+        private Long id;
+
+        // Project 상태
+        private String name;
+        private Type type;
+
+        private LocalDate startDate;
+        private LocalDate endDate;
+
+        private ProjectStatus status;
+        private Integer memberCount;
+        private List<ProjectMemberResponse.Summary> memberList;
+
+        // Lecture Project 상태
+        private String professor;
+        private DepartmentResponse department;
+        private List<LectureTimeResponse> lectureTimes;
+
+        // Side Project
+        private Field field;
+        private FieldCategory fieldCategory;
+
+        // Auditing 정보
+        private LocalDateTime createdDate;
+        private LocalDateTime lastModifiedDate;
+        private String createdBy;
+        private String lastModifiedBy;
+
+        public static Detail from(Project project) {
+            return from(project, null);
+        }
+
+        public static Detail from(Project project, List<ProjectMemberResponse.Summary> memberList) {
+            return project instanceof LectureProject ?
+                    from((LectureProject) project, memberList) :
+                    from((SideProject)    project, memberList);
+        }
+
+        public static Detail from(LectureProject lecture, List<ProjectMemberResponse.Summary> memberList) {
+            Detail dto = new Detail();
+
+            DepartmentResponse departmentResponse =     DepartmentResponse.from(lecture.getDepartment());
+            List<LectureTimeResponse> lectureTimes =    lecture.getLectureTimes().stream()
+                                                            .map(LectureTimeResponse::from).collect(Collectors.toList());
+
+            setProjectCommonDetails(lecture, dto, memberList);
+
+            // Lecture Project 상태
+            dto.setProfessor(lecture.getProfessor());
+            dto.setDepartment(departmentResponse);
+            dto.setLectureTimes(lectureTimes);
+
+            return dto;
+        }
+
+        public static Detail from(SideProject side, List<ProjectMemberResponse.Summary> memberList) {
+            Detail dto = new Detail();
+
+            setProjectCommonDetails(side, dto, memberList);
+
+            // Side Project 상태
+            dto.setField(side.getField());
+            dto.setFieldCategory(side.getFieldCategory());
+
+            return dto;
+        }
+
+        private static void setProjectCommonDetails(Project project, Detail dto, List<ProjectMemberResponse.Summary> memberList) {
+            dto.setId(project.getId());
+
+            // Project 상태
+            dto.setName(project.getName());
+            dto.setType(project.getType());
+            dto.setStatus(project.getStatus());
+
+            dto.setStartDate(project.getStartDate());
+            dto.setEndDate(project.getEndDate());
+
+            dto.setMemberCount(project.getMemberCount());
+
+            dto.setMemberList(Objects.requireNonNullElseGet(memberList, () -> project.getProjectMembers().stream()
+                    .map(ProjectMemberResponse.Summary::from).collect(Collectors.toList())));
+
+            // Auditing 정보
+            dto.setCreatedDate(project.getCreatedDate());
+            dto.setLastModifiedDate(project.getLastModifiedDate());
+            dto.setCreatedBy(project.getCreatedBy());
+            dto.setLastModifiedBy(project.getLastModifiedBy());
         }
 
     }
