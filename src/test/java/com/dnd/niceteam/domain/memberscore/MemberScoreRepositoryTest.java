f871a90d3e115225fc518f5dc0d4a5a831248016
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-22 14:13:13 +0900

    test: MemberScoreRepository.findByMember

---
+++ src/test/java/com/dnd/niceteam/domain/memberscore/MemberScoreRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.dnd.niceteam.domain.memberscore;
+
+import com.dnd.niceteam.common.TestJpaConfig;
+import com.dnd.niceteam.domain.account.Account;
+import com.dnd.niceteam.domain.account.AccountRepository;
+import com.dnd.niceteam.domain.code.Personality;
+import com.dnd.niceteam.domain.department.Department;
+import com.dnd.niceteam.domain.department.DepartmentRepository;
+import com.dnd.niceteam.domain.member.Member;
+import com.dnd.niceteam.domain.member.MemberRepository;
+import com.dnd.niceteam.domain.university.University;
+import com.dnd.niceteam.domain.university.UniversityRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Transactional
+@Import(TestJpaConfig.class)
+class MemberScoreRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private MemberScoreRepository memberScoreRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void findByMember() {
+        // given
+        University university = universityRepository.save(University.builder()
+                .name("테스트대학교")
+                .emailDomain("email.com")
+                .build());
+        Department department = departmentRepository.save(Department.builder()
+                .university(university)
+                .collegeName("테스트단과대학")
+                .name("테스트학과")
+                .region("서울")
+                .mainBranchType("본교")
+                .build());
+        MemberScore memberScore = memberScoreRepository.save(MemberScore.builder()
+                .level(1)
+                .reviewNum(0)
+                .totalParticipationScore(0)
+                .totalTeamAgainScore(0)
+                .build());
+        Account account = accountRepository.save(Account.builder()
+                .email("test@email.com")
+                .password("test-password")
+                .build());
+        Member member = memberRepository.save(Member.builder()
+                .account(account)
+                .university(university)
+                .department(department)
+                .memberScore(memberScore)
+                .nickname("test-nickname")
+                .admissionYear(2017)
+                .personality(new Personality(Personality.Adjective.LOGICAL, Personality.Noun.LEADER))
+                .introduction("")
+                .introductionUrl("")
+                .build());
+        em.flush();
+        em.clear();
+
+        // when
+        Member foundMember = memberRepository.findById(member.getId()).orElseThrow();
+        MemberScore foundMemberScore = memberScoreRepository.findByMember(foundMember).orElseThrow();
+
+        // then
+        assertThat(foundMember.getMemberScore()).isEqualTo(foundMemberScore);
+    }
+}
