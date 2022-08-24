e53711ed743a2fc412e2c1560792d6d85ae2cb78
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-24 23:02:14 +0900

    test: BookmarkRepository.findSideBookmarkDtoPageByMember

--- src/test/java/com/dnd/niceteam/domain/bookmark/BookmarkRepositoryTest.java
+++ src/test/java/com/dnd/niceteam/domain/bookmark/BookmarkRepositoryTest.java
@@ -5,19 +5,15 @@ import com.dnd.niceteam.domain.account.Account;
 import com.dnd.niceteam.domain.account.AccountRepository;
 import com.dnd.niceteam.domain.bookmark.dto.LectureBookmarkDto;
 import com.dnd.niceteam.domain.bookmark.dto.LectureTimeDto;
-import com.dnd.niceteam.domain.code.ActivityArea;
-import com.dnd.niceteam.domain.code.DayOfWeek;
-import com.dnd.niceteam.domain.code.Personality;
-import com.dnd.niceteam.domain.code.Type;
+import com.dnd.niceteam.domain.bookmark.dto.SideBookmarkDto;
+import com.dnd.niceteam.domain.code.*;
 import com.dnd.niceteam.domain.department.Department;
 import com.dnd.niceteam.domain.department.DepartmentRepository;
 import com.dnd.niceteam.domain.member.Member;
 import com.dnd.niceteam.domain.member.MemberRepository;
 import com.dnd.niceteam.domain.memberscore.MemberScore;
 import com.dnd.niceteam.domain.memberscore.MemberScoreRepository;
-import com.dnd.niceteam.domain.project.LectureProject;
-import com.dnd.niceteam.domain.project.LectureProjectRepository;
-import com.dnd.niceteam.domain.project.LectureTime;
+import com.dnd.niceteam.domain.project.*;
 import com.dnd.niceteam.domain.recruiting.Recruiting;
 import com.dnd.niceteam.domain.recruiting.RecruitingRepository;
 import com.dnd.niceteam.domain.university.University;
@@ -48,6 +44,9 @@ class BookmarkRepositoryTest {
 
     @Autowired
     private LectureProjectRepository lectureProjectRepository;
+
+    @Autowired
+    private SideProjectRepository sideProjectRepository;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -212,5 +211,131 @@ class BookmarkRepositoryTest {
         assertThat(lectureBookmarkDtoPage.getContent().get(1).getLectureTimes())
                 .extracting("dayOfWeek")
                 .containsOnly(DayOfWeek.WED, DayOfWeek.THUR);
+    }
+
+    @Test
+    void findSideBookmarkDtoPageByMember() {
+        // given
+        University university = universityRepository.save(University.builder()
+                .name("테스트대학교")
+                .emailDomain("email.com")
+                .build());
+        Department department = departmentRepository.save(Department.builder()
+                .university(university)
+                .collegeName("테스트단과대학")
+                .name("테스트학과")
+                .region("서울")
+                .mainBranchType("본교")
+                .build());
+        MemberScore memberScore1 = memberScoreRepository.save(MemberScore.builder()
+                .level(1)
+                .reviewNum(0)
+                .totalParticipationScore(0)
+                .totalTeamAgainScore(0)
+                .build());
+        Account account1 = accountRepository.save(Account.builder()
+                .email("test@email.com")
+                .password("test-password")
+                .build());
+        Member member1 = memberRepository.save(Member.builder()
+                .account(account1)
+                .university(university)
+                .department(department)
+                .memberScore(memberScore1)
+                .nickname("test-nickname")
+                .admissionYear(2017)
+                .personality(new Personality(Personality.Adjective.LOGICAL, Personality.Noun.LEADER))
+                .introduction("")
+                .introductionUrl("")
+                .build());
+        MemberScore memberScore2 = memberScoreRepository.save(MemberScore.builder()
+                .level(1)
+                .reviewNum(0)
+                .totalParticipationScore(0)
+                .totalTeamAgainScore(0)
+                .build());
+        Account account2 = accountRepository.save(Account.builder()
+                .email("test2@email.com")
+                .password("test-password2")
+                .build());
+        Member member2 = memberRepository.save(Member.builder()
+                .account(account2)
+                .university(university)
+                .department(department)
+                .memberScore(memberScore2)
+                .nickname("test-nickname2")
+                .admissionYear(2017)
+                .personality(new Personality(Personality.Adjective.LOGICAL, Personality.Noun.LEADER))
+                .introduction("")
+                .introductionUrl("")
+                .build());
+        SideProject doneProject = sideProjectRepository.save(SideProject.builder()
+                .name("테스트 프로젝트 1")
+                .startDate(LocalDate.of(2022, 3, 2))
+                .endDate(LocalDate.of(2022, 6, 30))
+                .field(Field.AD_MARKETING)
+                .fieldCategory(FieldCategory.CONTEST)
+                .build());
+        doneProject.end();
+        SideProject notStartedProject = sideProjectRepository.save(SideProject.builder()
+                .name("테스트 프로젝트 2")
+                .startDate(LocalDate.of(2022, 9, 2))
+                .endDate(LocalDate.of(2022, 12, 30))
+                .field(Field.IT_SW_GAME)
+                .fieldCategory(FieldCategory.EXTRA_ACTIVITY)
+                .build());
+        Recruiting recruiting1 = recruitingRepository.save(Recruiting.builder()
+                .project(doneProject)
+                .member(member1)
+                .title("title1")
+                .content("content1")
+                .recruitingMemberCount(4)
+                .recruitingType(Type.LECTURE)
+                .activityArea(ActivityArea.ONLINE)
+                .recruitingEndDate(LocalDate.of(2022, 3, 1))
+                .commentCount(0)
+                .bookmarkCount(0)
+                .poolUpCount(0)
+                .introLink("")
+                .build());
+        Recruiting recruiting2 = recruitingRepository.save(Recruiting.builder()
+                .project(notStartedProject)
+                .member(member2)
+                .title("title2")
+                .content("content2")
+                .recruitingMemberCount(2)
+                .recruitingType(Type.LECTURE)
+                .activityArea(ActivityArea.ONLINE)
+                .recruitingEndDate(LocalDate.of(2022, 4, 1))
+                .commentCount(0)
+                .bookmarkCount(0)
+                .poolUpCount(0)
+                .introLink("")
+                .build());
+        bookmarkRepository.save(Bookmark.builder()
+                .recruiting(recruiting1)
+                .member(member1)
+                .build());
+        bookmarkRepository.save(Bookmark.builder()
+                .recruiting(recruiting2)
+                .member(member1)
+                .build());
+        bookmarkRepository.save(Bookmark.builder()
+                .recruiting(recruiting1)
+                .member(member2)
+                .build());
+        em.flush();
+        em.clear();
+
+        // when
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<SideBookmarkDto> page = bookmarkRepository.findSideBookmarkDtoPageByMember(
+                pageable, member1);
+
+        // then
+        assertThat(page.getTotalElements()).isEqualTo(2);
+        assertThat(page.getContent()).hasSize(2);
+        assertThat(page.getContent()).extracting("title")
+                .containsExactly("title1", "title2");
     }
 }
