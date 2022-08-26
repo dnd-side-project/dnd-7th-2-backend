e18a011c7dd0553193ebb97af1eec5717c0ede3b
banan99 <banan99@naver.com>
2022-08-26 12:09:50 +0900

    feat(Controller, DTO) : Applicant 지원 현황 목록 조회 추가

--- src/main/java/com/dnd/niceteam/applicant/controller/ApplicantController.java
+++ src/main/java/com/dnd/niceteam/applicant/controller/ApplicantController.java
@@ -1,8 +1,10 @@
 package com.dnd.niceteam.applicant.controller;
 
 import com.dnd.niceteam.applicant.dto.ApplicantCreation;
+import com.dnd.niceteam.applicant.dto.ApplicantFind;
 import com.dnd.niceteam.applicant.service.ApplicantService;
 import com.dnd.niceteam.common.dto.ApiResult;
+import com.dnd.niceteam.common.dto.Pagination;
 import com.dnd.niceteam.security.CurrentUsername;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,27 +15,34 @@ import javax.validation.constraints.NotNull;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/recruiting/{recruitingId}/applicant")
+@RequestMapping("/recruiting")
 public class ApplicantController {
     private final ApplicantService applicantService;
 
-    @PostMapping
-    public ResponseEntity<ApiResult<ApplicantCreation.ResponseDto>> applicantAdd (@PathVariable @NotNull Long recruitingId,
-                                                                                  @CurrentUsername String username) {
+    @PostMapping("/{recruitingId}/applicant")
+    public ResponseEntity<ApiResult<ApplicantCreation.ResponseDto>> applicantAdd(@PathVariable @NotNull Long recruitingId,
+                                                                                 @CurrentUsername String username) {
         ApplicantCreation.ResponseDto responseDto = applicantService.addApplicant(recruitingId, username);
-
-        // TODO: 2022-08-18 팀원 등록도 추가 예정
 
         return new ResponseEntity<>(ApiResult.success(responseDto), HttpStatus.CREATED);
     }
 
-    @DeleteMapping
-    public ResponseEntity<ApiResult<Void>> applicantRemove (@PathVariable @NotNull Long recruitingId,
-                                                            @CurrentUsername String username) {
+    @DeleteMapping("/{recruitingId}/applicant")
+    public ResponseEntity<ApiResult<Void>> applicantRemove(@PathVariable @NotNull Long recruitingId,
+                                                           @CurrentUsername String username) {
         applicantService.removeApplicant(recruitingId, username);
 
         ApiResult<Void> apiResult = ApiResult.<Void>success().build();
         return ResponseEntity.ok(apiResult);
     }
 
+    @GetMapping("/applicant/me")
+    public ResponseEntity<ApiResult<Pagination<ApplicantFind.ListResponseDto>>> myApplyList(@RequestParam(defaultValue = "1", required = false) Integer page,
+                                                                                            @RequestParam(defaultValue = "10", required = false) Integer perSize,
+                                                                                            @RequestBody ApplicantFind.ListRequestDto requestDto,
+                                                                                            @CurrentUsername String username) {
+        Pagination<ApplicantFind.ListResponseDto> applications = applicantService.getMyApplicnts(page, perSize, requestDto, username);
+        ApiResult<Pagination<ApplicantFind.ListResponseDto>> apiResult = ApiResult.success(applications);
+        return ResponseEntity.ok(apiResult);
+    }
 }
