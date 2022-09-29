6f4e6eeab1880f0c20533cc8fde1c5e51dfa5de4
banan99 <banan99@naver.com>
2022-09-29 23:46:57 +0900

    refactor(RecruitingStatus) : EnumMapperType 구현하도록 수정

--- src/main/java/com/dnd/niceteam/domain/recruiting/RecruitingStatus.java
+++ src/main/java/com/dnd/niceteam/domain/recruiting/RecruitingStatus.java
@@ -1,7 +1,31 @@
 package com.dnd.niceteam.domain.recruiting;
 
-public enum RecruitingStatus {
-    IN_PROGRESS,
-    DONE,
-    FAILED
+import com.dnd.niceteam.domain.common.EnumMapperType;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum RecruitingStatus implements EnumMapperType {
+    IN_PROGRESS("진행중"),
+    DONE("완료"),
+    FAILED("실패")
+    ;
+    private final String title;
+    @Override
+    public String getCode() {
+        return name();
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonCreator
+    public static RecruitingStatus fromJson(@JsonProperty("code") String code) {
+        return valueOf(code);
+    }
 }
